82001a80ff5426e8c175a2491a94b6fc0c728e1b
shebz2023 <shebelleke@gmail.com>
2025-11-10 19:41:40 +0200

    chore:project setup

---
+++ lib/screens/landingPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Landingpage {
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Column(children: [Text('Welcome')]),
+    );
+  }
+}
